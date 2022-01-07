c8d38d5ac4f3584ab65d2acf535d2e2f50d9c3db
Rajat Saini <51861284+rajatsaini1@users.noreply.github.com>
2022-01-07 15:10:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,6 +40,12 @@ resource "aws_security_group" "prod-web-servers-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
